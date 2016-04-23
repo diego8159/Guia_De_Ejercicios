8d18711862f65b60da6f706c544732257d0f6b86
diego8159 <diegojmv59@gmail.com>
2016-04-23 12:23:22 -0300

    Aderido Ejercicio17

---
+++ Ejercicio Nro 17(Cuenta)/Ejercicio_17/Ejercicio_17/Cuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_17
+{
+    class Cuenta
+    {
+
+
+
+        public enum ETipoInteres()
+        {
+            
+        }
+    }
+}
---
+++ Ejercicio Nro 17(Cuenta)/Ejercicio_17/Ejercicio_17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Titulo
+            Console.Title= "Ejercicio 17";
+            
+            //Variables
+
+
+            //Ingreso de datos
+
+
+        }
+    }
+}
