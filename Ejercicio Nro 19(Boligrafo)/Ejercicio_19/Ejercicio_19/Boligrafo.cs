afc6d2734e2a9f71dd328907ca21f7a5fbd74e5f
diego8159 <diegojmv59@gmail.com>
2016-04-25 17:12:18 -0300

    Sin-Completar

---
+++ Ejercicio Nro 19(Boligrafo)/Ejercicio_19/Ejercicio_19/Boligrafo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_19
+{
+    class Boligrafo
+    {
+
+        private ConsoleColor color;
+        private int tinta;
+
+        public Boligrafo()
+        {
+            this.color = ConsoleColor.Blue;
+            this.tinta = 100;
+        }
+
+        public ConsoleColor getColor()
+        {
+            return this.color;
+        }
+
+        public int getTinta()
+        {
+            return this.tinta;
+        }
+
+        public bool Pintar(int gasto)
+        {
+            
+        }
+
+        public void Regargar()
+        { 
+            
+        }
+
+    }
+}
---
+++ Ejercicio Nro 19(Boligrafo)/Ejercicio_19/Ejercicio_19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 19";
+
+            int tinta;
+            ConsoleColor color;
+            Boligrafo boligrafo = new Boligrafo();
+
+            color = ConsoleColor.Blue;
+            tinta= boligrafo.getTinta();
+
+
+        }
+    }
+}
