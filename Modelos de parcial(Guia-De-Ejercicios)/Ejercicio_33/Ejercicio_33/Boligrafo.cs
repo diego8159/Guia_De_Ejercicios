150aed8b4337452f82325ec3040a0a94a936cdaf
diego8159 <diegojmv59@gmail.com>
2016-05-03 17:41:00 -0300

    Ejercicio_33(Completo)

---
+++ Modelos de parcial(Guia-De-Ejercicios)/Ejercicio_33/Ejercicio_33/Boligrafo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boligrafooo
+{
+    class Boligrafo
+    {
+        private string _marca;
+        private string _color;
+        private int _catidadTinta;
+
+        public Boligrafo(string color, string marca)
+        { 
+            this._catidadTinta= 0;
+            this._color = color;
+            this._marca = marca;
+        }
+
+        public Boligrafo(string color, string marca, int cantidadTinta):this(color, marca)
+        { }
+
+        public Boligrafo(int cantidadTinta, string color, string marca):this(color, marca, cantidadTinta)
+        { }
+
+        public Boligrafo(string marca, int cantidadTinta, string color):this(cantidadTinta, color, marca)
+        { }
+
+        public void Escribir(int cantidadNecesaria)
+        { 
+            this._catidadTinta= cantidadNecesaria;
+            if (this._catidadTinta < cantidadNecesaria)
+	        {
+		        this.Mostrar();
+	        }
+        }
+
+        private void Mostrar()
+        {
+            Console.WriteLine(this._color);
+            Console.WriteLine(this._marca);
+            Console.WriteLine(this._catidadTinta);
+            Console.WriteLine("*******************");
+        }
+
+        public static void MostrarBoligrafos(List<Boligrafo> ListaBoligrafos)
+        {
+            foreach (Boligrafo item in ListaBoligrafos)
+	        {
+		        item.Mostrar();
+	        } 
+        }
+
+        public static bool operator ==(Boligrafo unPen, Boligrafo otroPen)
+        {
+            bool respuesta;
+            return respuesta = (unPen._marca == otroPen._marca) && (unPen._color == otroPen._color);
+        }
+
+        public static bool operator !=(Boligrafo unPen, Boligrafo otroPen)
+        { 
+            return !(unPen == otroPen);
+        }
+
+        public static List<Boligrafo> operator +(List<Boligrafo> lista, Boligrafo unPen)
+        {
+            lista.Add(unPen);
+            return lista;
+        }
+
+        public static List<Boligrafo> operator -(List<Boligrafo> lista, Boligrafo unPen)
+        {
+            foreach (Boligrafo item in lista)
+            {
+                if (item == unPen)
+                {
+                    lista.Remove(unPen);
+                }
+            }
+
+            return lista;
+        }
+
+        public bool RegargarTinta()
+        {
+            bool retorno= false;
+
+             if (this._catidadTinta < 50)
+	        {
+		        this._catidadTinta= 100;
+                return retorno= true;
+	        }
+             else
+                 return retorno;
+        }
+
+        public bool RegargarTinta(int cantidad)//:this()
+        { 
+            bool retorno= false;
+
+            if (this._catidadTinta > 50)
+	        {
+                this._catidadTinta = cantidad;
+                return retorno= true;
+	        }
+            else
+            {
+                this.RegargarTinta();
+                return retorno;
+            }
+        }
+
+
+    }
+}
---
+++ Modelos de parcial(Guia-De-Ejercicios)/Ejercicio_33/Ejercicio_33/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boligrafooo;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 33 Modelo de parcial-1";
+
+            List<Boligrafo> miLista = new List<Boligrafo>();
+            Boligrafo Unboligrafo = new Boligrafo("blanco", "bic", 60);
+            Unboligrafo.Escribir(70);
+
+            if (Unboligrafo.RegargarTinta(90))
+                Console.WriteLine("Boligrafo Recargado");
+            else 
+                Console.WriteLine("No se pudo recargar");
+
+            Unboligrafo.Escribir(30);
+
+            if (Unboligrafo.RegargarTinta())
+                Console.WriteLine("Boligrafo Recargado");
+            else 
+                Console.WriteLine("No se pudo recargar");
+
+            miLista = miLista + Unboligrafo;
+
+            Boligrafo boligrafo2 = new Boligrafo("rojo", "Silvapen", 50);
+            miLista.Add(boligrafo2); Console.WriteLine("               Muestro :");
+            Boligrafo.MostrarBoligrafos(miLista); Console.ReadLine();
+            Console.WriteLine("                Sumo :");
+            Boligrafo Otrobligrafo = new Boligrafo("verde", "Parker", 60);
+            miLista = miLista + Otrobligrafo; Boligrafo.MostrarBoligrafos(miLista);
+            Console.ReadLine(); Console.WriteLine("                 Resto :");
+            Boligrafo BoligrafoTres = new Boligrafo(40, "verde", "Parker");
+            miLista = miLista - BoligrafoTres; Boligrafo.MostrarBoligrafos(miLista);
+            Console.ReadLine();
+
+        }
+    }
+}
